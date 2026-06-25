2577a09223c2225bcc6cbe4f3492a1367c304de8
koreanasset <a20705032@gmail.com>
2026-06-25 21:23:11 +0900

    Add temporary debug endpoint to diagnose missing env vars

--- api/cron-stock-brief.js
+++ api/cron-stock-brief.js
@@ -130,6 +130,19 @@ async function buildAiContent(volumeList, changeList, dateLabel) {
 }
 
 export default async function handler(req, res) {
+  if (req.query.debug === "1") {
+    res.status(200).json({
+      hasAppKey: !!process.env.KIWOOM_APP_KEY,
+      appKeyLen: (process.env.KIWOOM_APP_KEY || "").length,
+      hasAppSecret: !!process.env.KIWOOM_APP_SECRET,
+      appSecretLen: (process.env.KIWOOM_APP_SECRET || "").length,
+      hasCronSecret: !!process.env.CRON_SECRET,
+      hasServiceRoleKey: !!process.env.SUPABASE_SERVICE_ROLE_KEY,
+      hasAnthropicKey: !!process.env.ANTHROPIC_API_KEY,
+    });
+    return;
+  }
+
   const cronSecret = process.env.CRON_SECRET;
   if (cronSecret) {
     const auth = req.headers.authorization || "";
